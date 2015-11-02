f89ffbe7b7112b6e52a0506510be1062fdf6bd1d
Tokimon <tokevoltelen@gmail.com>
2015-11-02 10:51:29 +0100

    Changed grid validation method, to use the Sudoku solver validation method.

--- sudoku-solver.spec.js
+++ sudoku-solver.spec.js
@@ -17,46 +17,70 @@ import {
 import kudoku from './kudoku.js';
 import Sudoku from './Sudoku.js';
 
-function gridToKudoku( grid ) {
+function validateGrid( grid ) {
+  const SudokuValidator = new Sudoku(grid);
+  return SudokuValidator.validate();
+}
+
+function gridToFlat( grid ) {
   return Sudoku.flatten(grid).replace(/0/g, '.');
 }
 
-function kudokuToGrid( kudokuStr ) {
+function faltToGrid( kudokuStr ) {
   return kudokuStr
       .match(/.{9}/g)
       .map(( part ) => { return part.match(/./g); });
 }
 
+function kudokuResolve( grid ) {
+  const kudokuResult = kudoku()(gridToFlat(grid), 1);
+  return faltToGrid(gridToFlat(kudokuResult));
+}
+
+describe('Sudoku validation', function() {
+  it('should validate Grid 1 as correct', function() {
+    expect(validateGrid(grid1_solved)).to.be.true;
+  });
+
+  it('should validate Grid 2 as correct', function() {
+    expect(validateGrid(grid2_solved)).to.be.true;
+  });
+
+  it('should validate Extreme grid as correct', function() {
+    expect(validateGrid(extreme_solved)).to.be.true;
+  });
+});
+
 describe('Kudoku solver (reference solver)', function() {
   it('should solve grid 1', function() {
-    const result = gridToKudoku(kudoku()(gridToKudoku(grid1), 1));
-    expect(gridToKudoku(grid1_solved)).to.equal(result);
+    const result = kudokuResolve(grid1);
+    expect(validateGrid(result)).to.be.true;
   });
 
   it('should solve grid 2', function() {
-    const result = gridToKudoku(kudoku()(gridToKudoku(grid2), 1));
-    expect(gridToKudoku(grid2_solved)).to.equal(result);
+    const result = kudokuResolve(grid2);
+    expect(validateGrid(result)).to.be.true;
   });
 
   it('should solve extreme', function() {
-    const result = gridToKudoku(kudoku()(gridToKudoku(extreme), 1));
-    expect(gridToKudoku(extreme_solved)).to.equal(result);
+    const result = kudokuResolve(extreme);
+    expect(validateGrid(result)).to.be.true;
   });
 });
 
 describe('Sudoku Solver', function() {
   it(`should solve grid 1`, function() {
     const sudoku = new Sudoku(grid1);
-    expect(gridToKudoku(sudoku.solve())).to.equal(gridToKudoku(grid1_solved));
+    expect(validateGrid(sudoku.solve())).to.be.true;
   });
 
   it(`should solve grid 2`, function() {
     const sudoku = new Sudoku(grid2);
-    expect(gridToKudoku(sudoku.solve())).to.equal(gridToKudoku(grid2_solved));
+    expect(validateGrid(sudoku.solve())).to.be.true;
   });
 
   it(`should solve extreme`, function() {
     const sudoku = new Sudoku(extreme);
-    expect(gridToKudoku(sudoku.solve())).to.equal(gridToKudoku(extreme_solved));
+    expect(validateGrid(sudoku.solve())).to.be.true;
   });
 });
